0eca4dcd441c9b6c7f137726ac3321ef4ca8723f
JacksonWong <jackson.cw.wong@gmail.com>
2015-07-07 17:16:37 -0400

    first full attempt at ui.bootstrap modal

--- modalCtrl.js
+++ modalCtrl.js
@@ -1,5 +1,13 @@
-function ModalCtrl() {
-
+function ModalCtrl($modalInstance) {
+	this.modalInstance = $modalInstance;
 }
 
 angular.module('userApp').controller('ModalCtrl', ModalCtrl);
+
+ModalCtrl.prototype.confirm = function confirm() {
+	this.modalInstance.close();
+};
+
+ModalCtrl.prototype.cancel = function cancel() {
+	this.modalInstance.dismiss();
+};
